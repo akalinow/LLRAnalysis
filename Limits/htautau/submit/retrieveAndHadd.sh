01a5ac682d77cc7b79eced75a93f7d3a43b1c892
paganini <>
2013-02-21 16:46:42 +0000

    copying Bianchi B53X branch

--- Limits/htautau/submit/retrieveAndHadd.sh
+++ Limits/htautau/submit/retrieveAndHadd.sh
@@ -8,15 +8,15 @@ do
 
 echo ">>>>>>> Now doing eleTau"
 
-#crab -status -c crab_eleTau_SM_diTauNSVfitMass_mH${mass}
-#crab -getoutput -c crab_eleTau_SM_diTauNSVfitMass_mH${mass}
-#hadd -f crab_eleTau_SM_diTauNSVfitMass_mH${mass}/res/eleTau_SM_diTauNSVfitMass_mH${mass}.root crab_eleTau_SM_diTauNSVfitMass_mH${mass}/res/eleTau_SM_diTauNSVfitMass_mH${mass}_*.root
-#cp crab_eleTau_SM_diTauNSVfitMass_mH${mass}/res/eleTau_SM_diTauNSVfitMass_mH${mass}.root grid/
+crab -status -c crab_eleTau_SM_diTauNSVfitMass_mH${mass}
+crab -getoutput -c crab_eleTau_SM_diTauNSVfitMass_mH${mass}
+hadd -f crab_eleTau_SM_diTauNSVfitMass_mH${mass}/res/eleTau_SM_diTauNSVfitMass_mH${mass}.root crab_eleTau_SM_diTauNSVfitMass_mH${mass}/res/eleTau_SM_diTauNSVfitMass_mH${mass}_*.root
+cp crab_eleTau_SM_diTauNSVfitMass_mH${mass}/res/eleTau_SM_diTauNSVfitMass_mH${mass}.root grid/
 
-#crab -status -c crab_eleTau_SM_diTauVisMass_mH${mass}
-#crab -getoutput -c crab_eleTau_SM_diTauVisMass_mH${mass}
-#hadd -f crab_eleTau_SM_diTauVisMass_mH${mass}/res/eleTau_SM_diTauVisMass_mH${mass}.root crab_eleTau_SM_diTauVisMass_mH${mass}/res/eleTau_SM_diTauVisMass_mH${mass}_*.root
-#cp crab_eleTau_SM_diTauVisMass_mH${mass}/res/eleTau_SM_diTauVisMass_mH${mass}.root grid/
+crab -status -c crab_eleTau_SM_diTauVisMass_mH${mass}
+crab -getoutput -c crab_eleTau_SM_diTauVisMass_mH${mass}
+hadd -f crab_eleTau_SM_diTauVisMass_mH${mass}/res/eleTau_SM_diTauVisMass_mH${mass}.root crab_eleTau_SM_diTauVisMass_mH${mass}/res/eleTau_SM_diTauVisMass_mH${mass}_*.root
+cp crab_eleTau_SM_diTauVisMass_mH${mass}/res/eleTau_SM_diTauVisMass_mH${mass}.root grid/
 
 echo ">>>>>>> Now doing muTau"
 
