5e9a06a48dc8f4c0ccb51959a862e6e10bcc658f
ndaci <>
2013-03-07 14:53:57 +0000

    Using *HTT* to generate.

--- Utilities/test/tools/genAll.sh
+++ Utilities/test/tools/genAll.sh
@@ -1,4 +1,4 @@
-ls HTT_* | awk -F ":" '{print "nohup ./checkTask.sh  "$1" noforce  pub > "$1"/log/launcher.txt &"}' | grep "HTT" > checkAll.sh
-ls HTT_* | awk -F ":" '{print "nohup ./checkTask.sh  "$1" force  pub > "$1"/log/launcher.txt &"}' | grep "HTT" > checkAllForce.sh
-ls HTT_* | awk -F ":" '{print "nohup ./checkTask.sh  "$1" forceAll  pub > "$1"/log/launcher.txt &"}' | grep "HTT" > checkAllForceAll.sh
+ls *HTT* | awk -F ":" '{print "nohup ./checkTask.sh  "$1" noforce  pub > "$1"/log/launcher.txt &"}'  | grep "HTT" > checkAll.sh
+ls *HTT* | awk -F ":" '{print "nohup ./checkTask.sh  "$1" force  pub > "$1"/log/launcher.txt &"}'    | grep "HTT" > checkAllForce.sh
+ls *HTT* | awk -F ":" '{print "nohup ./checkTask.sh  "$1" forceAll  pub > "$1"/log/launcher.txt &"}' | grep "HTT" > checkAllForceAll.sh
 chmod u+x checkAll*.sh 
