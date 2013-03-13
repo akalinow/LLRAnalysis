a183b1c7ec6d8d4c1b31c0a3e69d448b0bf094cb
inaranjo <>
2013-03-13 19:16:39 +0000

    temp dir

--- Limits/bin/makeTreeSkimmerElecTau_Spring13.py
+++ Limits/bin/makeTreeSkimmerElecTau_Spring13.py
@@ -7,7 +7,7 @@ import string
 import subprocess
 
 WorkdirLoc = '/data_CMS/cms/htautau/PostMoriond/TREES/EleTau/'
-OutDir   = '/data_CMS/cms/htautau/PostMoriond/NTUPLES/EleTau/IvoTest/'
+OutDir   = '/data_CMS/cms/htautau/PostMoriond/NTUPLES/EleTau/temp/'
 
 options = {
     ###e+tau samples
