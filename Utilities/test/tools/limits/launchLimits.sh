0b1de70a83e5bde023efce20ed2d28bf2aed0f1c
ndaci <>
2013-04-02 14:51:56 +0000

    Fix 2nd argument : should be a label e.g. LLR_XXX.

--- Utilities/test/tools/limits/launchLimits.sh
+++ Utilities/test/tools/limits/launchLimits.sh
@@ -11,19 +11,19 @@ echo "Install directories + add bin-by-bin uncertainties"
 python HiggsAnalysis/HiggsToTauTau/scripts/doSM.py -a bin-by-bin -c $1 -p 8TeV --update-all --inputs-$1=$2 --label $2 90-160:5 
 
 echo "Compute the pulls"
-for((i=90;i<=160;i+=5)) do limit.py --stable --max-likelihood $2/bin-by-bin/$1/$i ; done 
+for((i=90;i<=160;i+=5)) do limit.py --stable --max-likelihood LIMITS_$2/bin-by-bin/$1/$i ; done 
 
 echo "generate and launch jobs to compute the limits"
-submit.py --asymptotic $2/bin-by-bin/$1/* --llr --jobdir $2
-cd $2/bin-by-bin-$1/
+submit.py --asymptotic LIMITS_$2/bin-by-bin/$1/* --llr --jobdir LIMITS_$2
+cd LIMITS_$2/bin-by-bin-$1/
 ./bin-by-bin-$1_submit.sh
 cd ../..
 
 echo "prepare the limits comparison"
-cp -r $2/bin-by-bin/$1/ $2/bin-by-bin/$1_$3
+cp -r LIMITS_$2/bin-by-bin/$1/ LIMITS_$2/bin-by-bin/$1_$3
 
 echo "plot the limits"
-plot --asymptotic HiggsAnalysis/HiggsToTauTau/python/layouts/limit-sm.py $2/bin-by-bin/$1_$3
-mkdir $2/bin-by-bin/$1_$3/results
-mv $1_$3_limit.* $2/bin-by-bin/$1_$3/results
+plot --asymptotic HiggsAnalysis/HiggsToTauTau/python/layouts/limit-sm.py LIMITS_$2/bin-by-bin/$1_$3
+mkdir LIMITS_$2/bin-by-bin/$1_$3/results
+mv $1_$3_limit.* LIMITS_$2/bin-by-bin/$1_$3/results
 
