bf9e1c02c69122b38901a5117146d2dbea83c52f
ndaci <>
2013-04-08 13:20:13 +0000

    Use checkValidityFiles.py to generate batch/resubmit.sh to resubmit invalid files.

---
+++ Limits/bin/checkValidityFiles.sh
@@ -0,0 +1,5 @@
+# checkValidityFiles.sh
+# arg 1 : directory e.g. /data_CMS/cms/htautau/PostMoriond/NTUPLES/EleTau/temp/SoftAnalysis/
+# arg 2 : tag (EleTau / MuTau)
+
+python checkValidityFiles.py $1 &> log_check_$2.txt ; grep "not closed" log_check_$2.txt | awk '{print $4}' | awk -F "/nTuple" '{print $2}' | awk -F "." '{print "/opt/exp_soft/cms/t3/t3submit -V job_"$1".sh"}' > batch/resubmit_$2.sh ; chmod u+x batch/resubmit_$2.sh
