719bb831e0e0078760eb7084ebc019f17115f55d
ndaci <>
2013-04-19 08:49:13 +0000

    Change string.

--- Utilities/test/tools/launchCopyReports.sh
+++ Utilities/test/tools/launchCopyReports.sh
@@ -1,1 +1,4 @@
-ls $1/*/res/lumiSummary.json | grep "Data" | awk -v dir=$1 -F "/" '{print "cp "$dir"/"$1"/"$2"/"$3" /data_CMS/cms/htautau/JSON/report/report_"$1".txt"}' > copyReports.sh ; chmod u+x copyReports.sh ; ./copyReports.sh ; rm copyReports.sh
+ls $1/*/res/lumiSummary.json | grep "Data" | awk -v dir=$1 -F "//" '{print "cp "$dir"/"$2" /data_CMS/cms/htautau/JSON/report/report_"$2".txt"}' 
+
+
+#> copyReports.sh ; chmod u+x copyReports.sh ; ./copyReports.sh ; rm copyReports.sh
