d152152a392f335e08c85841c7161a6af78cf555
ndaci <>
2013-04-19 09:03:44 +0000

    Add automatic creation of launch.sh to be slightly modified by hand afterwards.

--- Utilities/test/tools/copy_all_subfolders_mkdir_L1_noDoublons.sh
+++ Utilities/test/tools/copy_all_subfolders_mkdir_L1_noDoublons.sh
@@ -3,3 +3,5 @@ rfdir $1 | awk '{print "mkdir "dir"/"$9" ; ./dorfcp_noDoublon.sh "dpm"/"$9"/ "di
 chmod u+x copy_$3.sh
 ./copy_$3.sh > do_copy_$3.sh
 chmod u+x do_copy_$3.sh
+ls | grep HTT | awk '{print "cd .. ; cd "$1" ; nohup ./copy.sh &"}' > launch.sh
+chmod u+x launch.sh
