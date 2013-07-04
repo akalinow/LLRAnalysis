2fb091cbf9afafbd74907b581f8a54f12dc9b600
Artur Kalinowski <Artur.Kalinowski@cern.ch>
2013-07-04 09:04:52 +0200

    test 2

--- TauTauStudies/test/job.sh
+++ TauTauStudies/test/job.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+#AK test2
+
 export WORKINGDIR="/home/llr/cms/lbianchini/CMSSW_5_2_2/src/LLRAnalysis/TauTauStudies/test/"
 export SCRAM_ARCH=slc5_amd64_gcc462
 source /opt/exp_soft/cms/cmsset_default.sh
