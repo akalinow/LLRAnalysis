dc071de022ed28bcdef045c386b930d0a6ca3615
Christian <christian.veelken@cern.ch>
2014-04-15 12:02:55 +0200

    adding missing files

---
+++ HadTauStudies/interface/triggerTurnOnCurves.h
@@ -0,0 +1,20 @@
+#ifndef LLRAnalysis_HadTauStudies_triggerTurnOnCurves_h
+#define LLRAnalysis_HadTauStudies_triggerTurnOnCurves_h
+
+double eff2012IsoParkedTau19fb_Simone(double, double);
+double eff2012IsoParkedTau19fbMC_Simone(double, double);
+double eff2012IsoTau19fb_Simone(double, double);
+double eff2012IsoTau19fbMC_Simone(double, double);
+double eff2012Jet19fb(double, double);
+
+double effDrop2012IsoParkedTau_Arun_cutMedium(double, double);
+double eff2012IsoParkedTau_Arun_cutMedium(double, double);
+double effDrop2012IsoParkedTau_Arun_mvaTight(double, double);
+double eff2012IsoParkedTau_Arun_mvaTight(double, double);
+double effDrop2012IsoParkedTau_Arun_mvaVTight(double, double);
+double eff2012IsoParkedTau_Arun_mvaVTight(double, double);
+double eff2012IsoParkedTauMC_Arun_cutMedium(double, double);
+double eff2012IsoParkedTauMC_Arun_mvaTight(double, double);
+double eff2012IsoParkedTauMC_Arun_mvaVTight(double, double);
+
+#endif
